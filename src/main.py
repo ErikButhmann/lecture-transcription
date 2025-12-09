d2ba860d19b680272892d414c3aab8d9c667251e
ErikButhmann <erik.buthmann@gmail.com>
2025-12-09 14:17:36 +0100

    additional comments, tify up code

--- src/main.py
+++ src/main.py
@@ -14,41 +14,41 @@ def transcribe_video(video_location: pl.Path,
     Args:
         video_location (Path):          path to the input video.
         chunk_duration (int):           size of the audio chunks in seconds. The function will split the input video's audio
-                                        into chunks after the first <chunk_size> seconds, after the second <chunk_size>
-                                        seconds etc. These chunks will be send to the openai API for transcription. This is
-                                        necessary since the API cannot deal with files that are larger than 25MB. Therefore,
-                                        you must make sure that the resulting parts do not exceed this size. The default of
-                                        900 seconds equals 15 minutes and most often ensures that the resulting chunks are
-                                        below 25 MB.
-        output_location (Path | None):  output file to save the transcription in. If None, transcription will be saved
+                                        into chunks. Split points will be set after the first <chunk_size> seconds, after the
+                                        second <chunk_size> seconds etc. These chunks will be send to the openai API for transcription.
+                                        This splitting is necessary since the API cannot deal with files that are larger
+                                        than 25MB. Therefore, you must make sure that the resulting parts (chunks) do not exceed
+                                        this size. The default of 900 seconds equals 15 minutes and most often ensures that the
+                                        resulting chunks are below 25 MB.
+        output_location (Path | None):  path to the output file to save the transcription in. If None, transcription will be saved
                                         in the same directory as the input video under the same name in the .txt format.
     '''
     # creating file and directory names
-    audio_file_location = create_file_in_same_dir(input_file=video_location, file_ending='_audio.mp3')
-    # audio_file_location = video_location.parent
-    # file_name = video_location.parts[-1]
-    # file_name = file_name[0:file_name.rfind('.')] + '_audio.mp3'
-    # audio_file_location = audio_file_location / file_name
+    audio_file_location = create_file_in_same_dir(input_file_location=video_location, file_ending='_audio.mp3')
     file_name = video_location.parts[-1]
     chunk_file_folder = audio_file_location.parent / file_name[0:file_name.rfind('.')]
     chunk_file_folder.mkdir()
 
     if output_location is None:
-        output_location = create_file_in_same_dir(input_file=video_location, file_ending='_transcript.txt')
+        output_location = create_file_in_same_dir(input_file_location=video_location, file_ending='_transcript.txt')
 
-    extract_audio(video=video_location, output_location=audio_file_location)
-    create_audio_chunks(audio = audio_file_location, chunk_duration = chunk_duration, output_location = chunk_file_folder)
+    extract_audio(video_location=video_location, output_location=audio_file_location)
+    create_audio_chunks(audio_location = audio_file_location, chunk_duration = chunk_duration, output_location = chunk_file_folder)
 
+    # iterate over all mp3 files in the chunk folder. Each file is a single chunk
     for audio_chunk_number, audio_chunk in enumerate(list(chunk_file_folder.glob('**/*.mp3'))):
 
         print('Creating transcription for chunk ' + str(audio_chunk_number))
-        chunk_transcription = transcribe_audio(audio=audio_chunk)
+        chunk_transcription = transcribe_audio(audio_location=audio_chunk)
 
         with open(output_location, 'a') as f:
             f.write('PART ' + str(audio_chunk_number) + '\n')
 
         current_time_offset = audio_chunk_number * chunk_duration
-        for segment in chunk_transcription.segments:
+        for segment in chunk_transcription.segments: # type: ignore
+            # we need to add the current time offset to get the actual time of the original video
+            # if we do not do this, the resulting timestamps will start counting at the beginning
+            # of each chunk
             print_time = time.strftime("%H:%M:%S", time.gmtime(segment.start + current_time_offset))
             with open(output_location, 'a') as f:
                 f.write(print_time + ' ' + segment.text + '\n')
--- src/process_video.py
+++ src/process_video.py
@@ -1,61 +1,47 @@
-from moviepy import AudioFileClip
 import pathlib as pl
+
+from moviepy import AudioFileClip
 
 from utils import create_file_in_same_dir
 
-def extract_audio(video: pl.Path, output_location: pl.Path | None = None) -> None:
+def extract_audio(video_location: pl.Path, output_location: pl.Path | None = None) -> None:
     '''
     Extract the audio of an input video and save it.
     
     Args:
-        video (Path):                   path to the input video. The function will extract this video's audio.
+        video_location (Path):          path to the input video. The function will extract this video's audio.
         output_location (Path | None):  output file to save the video's audio in. If None, audio will be saved
                                         in the same directory as the video under the same name in the .mp3 format.
     '''
     # reading video as audio file
-    # this will automatically transform it into a wave file
-    clip = AudioFileClip(video)
+    clip = AudioFileClip(video_location)
 
     # creating default output file if not provided
     if output_location is None:
-        output_location = create_file_in_same_dir(input_file=video, file_ending='_audio.mp3')
-        # # using name of video as name for audio
-        # file_name = video.parts[-1]
-        # file_name = file_name[0:file_name.rfind('.')] + '_audio.mp3'
-
-        # # accessing same path as input and adding file_name
-        # output_location = video.parent
-        # output_location = output_location / file_name
+        output_location = create_file_in_same_dir(input_file_location=video_location, file_ending='_audio.mp3')
 
     # writing audio to file
     clip.write_audiofile(output_location)
     clip.close()
 
 
-def clip_audio(audio: pl.Path, start: int = 0, end:int =60, output_location: pl.Path | None = None) -> None:
+def clip_audio(audio_location: pl.Path, start: int = 0, end:int =60, output_location: pl.Path | None = None) -> None:
     '''
     Clip an input audio file to the seconds between the parameters start and end. Save the resulting
     audio clip in the provided output_location or a default folder.
 
-    audio (Path):                    path to the input audio.
-    start (int):                     start time to cut in seconds
-    end (int):                       end time to cut in seconds
-    output_location (Path | None):   output file to save the clipped audio in. If None, it will be saved
-                                        in the same directory as the input under the same name in the .mp3 format.
+    audio_location (Path):          path to the input audio.
+    start (int):                    start time of the cut in seconds
+    end (int):                      end time of the cut in seconds
+    output_location (Path | None):  output file to save the clipped audio in. If None, it will be saved
+                                    in the same directory as the input under the same name in the .mp3 format.
     '''
-    clip = AudioFileClip(audio)
+    clip = AudioFileClip(audio_location)
     sub_clip = clip.subclipped(start_time=start, end_time=end)
 
     # creating default output file if not provided
     if output_location is None:
-        output_location = create_file_in_same_dir(input_file=audio, file_ending='_clipped.mp3')
-        # # using name of video as name for audio
-        # file_name = audio.parts[-1]
-        # file_name = file_name[0:file_name.rfind('.')] + '_clipped.mp3'
-
-        # # accessing same path as input and adding file_name
-        # output_location = audio.parent
-        # output_location = output_location / file_name
+        output_location = create_file_in_same_dir(input_file_location=audio_location, file_ending='_clipped.mp3')
 
     # writing audio to file
     sub_clip.write_audiofile(output_location)
@@ -63,29 +49,31 @@ def clip_audio(audio: pl.Path, start: int = 0, end:int =60, output_location: pl.
     clip.close()
 
 
-def create_audio_chunks(audio: pl.Path, chunk_duration: int, output_location: pl.Path | None = None) -> None:
+def create_audio_chunks(audio_location: pl.Path, chunk_duration: int, output_location: pl.Path | None = None) -> None:
     '''
-    Splits input audio file in smaller files. This is often necessary since the openai API cannot deal with
+    Splits the input audio file in smaller files. This is often necessary since the openai API cannot deal with
     audio files that are larger than 25MB.
 
     Args:
-        audio (Path):                   input audio file to split in chunks.
+        audio_location (Path):          input audio file to split in chunks.
         chunk_duration (int):           size of the audio chunks in seconds. The function will split the input audio file
-                                        into chunks after the first <chunk_size> seconds, after the second <chunk_size>
+                                        into chunks after the first <chunk_duration> seconds, after the second <chunk_duration>
                                         seconds etc.
-        output_location (Path | None):  output directory to save the chunks in. If values is None, a new sub-directory
+        output_location (Path | None):  output directory to save the chunks in. If value is None, a new sub-directory
                                         will be created in the directory of the input audio file. This sub-directory is
                                         named after the input audio file. The resulting audio files will be .mp3 files.
+                                        All these chunk files will be named after the input file with additional '_part0',
+                                        '_part1' etc. extensions.
     '''
-    clip = AudioFileClip(audio)
+    clip = AudioFileClip(audio_location)
     overall_duration = clip.duration
     clip.close()
 
     # creating sub-directory for chunks
     if output_location is None:
-        subdir_name = audio.parts[-1]
+        subdir_name = audio_location.parts[-1]
         subdir_name = subdir_name[0:subdir_name.rfind('.')]
-        output_location = audio.parent / subdir_name
+        output_location = audio_location.parent / subdir_name
         output_location.mkdir()
     if not output_location.is_dir():
         raise ValueError('Output location must be a directory.')
@@ -100,17 +88,18 @@ def create_audio_chunks(audio: pl.Path, chunk_duration: int, output_location: pl
         if current_end > overall_duration:
             # if the increased end is higher than the overall duration
             # this means that we reached the end of the main file.
-            # Therefore, the end time is set to the overall duration since the main
-            # audio cannot be clipped to an end behind this duration
+            # Therefore, the end time is set to the overall duration in
+            # this case since the main audio cannot be clipped to an end
+            # behind this duration
             current_end = overall_duration
 
         # creating a file name for each chunk
         # file names are numbered based on the chunk number
-        file_name = audio.parts[-1]
+        file_name = audio_location.parts[-1]
         file_name = file_name[0:file_name.rfind('.')] + '_part' + str(current_chunk) + '.mp3'
 
         # clipping the main audio file
-        clip_audio(audio=audio, start=current_start, end=current_end, output_location = output_location / file_name)
+        clip_audio(audio_location=audio_location, start=current_start, end=current_end, output_location = output_location / file_name)
 
         # increase start and end time of the next chunk
         current_start += chunk_duration
