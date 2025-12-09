3aa0e0b158c7fc50e787dec06524394069f27214
ErikButhmann <erik.buthmann@gmail.com>
2025-12-09 14:32:43 +0100

    added possibility to provide ai model with correct spelling

--- src/transcribe_ai.py
+++ src/transcribe_ai.py
@@ -5,14 +5,17 @@ from openai import OpenAI
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
-def transcribe_audio(audio_location: pl.Path) -> BaseModel:
+def transcribe_audio(audio_location: pl.Path, spelling: str = '') -> BaseModel:
     '''
     Transcribes the content of an audio file and saves the transcription. This function uses the openai API.
     Therefore, an openai API key is necessary. This key should be saved in a .env-file in the root folder.
     This file must contain a variable called OPENAI_API_KEY that is associated with a working key.
 
     Args:
-        audio_location (Path):   input audio file to transcribe
+        audio_location (Path):  input audio file to transcribe
+        spelling (str):         a string containing a list of unfamiliar words with their correct spelling.
+                                This can improve the quality of the whisper model since it often faces problems
+                                with unfamiliar or unusual words. Default is the empty string.
     '''
     load_dotenv()
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -23,7 +26,8 @@ def transcribe_audio(audio_location: pl.Path) -> BaseModel:
             model='whisper-1',
             language='en',
             response_format='verbose_json',
-            timestamp_granularities=["segment"]
+            timestamp_granularities=["segment"],
+            prompt=spelling
         )
 
     return transcript
