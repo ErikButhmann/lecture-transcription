4a8a232f5373b446a2a89b658d5baed1eb3c64d6
ErikButhmann <erik.buthmann@gmail.com>
2025-12-11 13:34:07 +0100

    function to transcribe pptx files enables differentiation by slide

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # Transcribe a lecture video
 
-The code provided in this repository allows to transcribe a lecture video (or another kind of video, but this has not been tested).
+The code provided in this repository allows to transcribe a lecture video (or another kind of video, but this has not been tested) or a presentation with audio comments.
 It makes use of OpenAI's [transriptions API](https://platform.openai.com/docs/guides/speech-to-text).
 
 ## General Workflow
 
+### Video Transcription
 1. Load the video, for example a .mp4 file and extract its audio.
 2. Split the audio in smaller chunks since the API cannot work with audio files larger than 25 MB.
 3. Send each chunk to the API and get the transcription.
 4. Combine the transcriptions of the chunks in one text-file.
+
+### Presentation Transcription
+1. Copy the .pptx-file and transform the copy into a .zip-file.
+2. Send the audio-files in the .zip archive to the API and get the transcriptions.
+3. Combine the slides' transcriptions in one text-file.
 
 ## Setup
 
@@ -16,7 +22,7 @@ It makes use of OpenAI's [transriptions API](https://platform.openai.com/docs/gu
 2. (optional but recommended) Create a virtual environment. [venv](https://docs.python.org/3/library/venv.html) was used when writing the code.
 3. Install dependencies using `pip install -r requirements.txt`
 4. Add your [OpenAI-Key](https://platform.openai.com/docs/overview) in a .env file in the root folder. This .env file should contain a variable called `OPENAI_API_KEY`. I.e. you must add the following line to the .env-file `OPENAI_API_KEY=<Your_Key>`. This key will be used to access OpenAI's services.
-5. The function `transcribe_video` in the [main.py](src/main.py) module provides the main interface to transcribe a video. You can input a file path to an existing video. The resulting transcription will be created in the same directory as a .txt file.
+5. The functions `transcribe_video` and `transcribe_presentation` in the [main.py](src/main.py) module provide the main interfaces to transcribe a video or a presentation (.pptx-file). You can input a file path to an existing video or a .pptx-file. The resulting transcription will be created in the same directory as a .txt file. For a video the transcription will contain timestamps. For a presentation the transcription will contain timestamps and indicators to which slide a piece of text belongs. However, if there are slides without audio in the presentation, the slide count in the transcript deviates from the slide count in the presentation.
 
 ## Disclaimer
 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 import pathlib as pl
+import shutil
+import zipfile
+import os
 import time
+
+from moviepy import AudioFileClip
 
 from process_video import extract_audio, create_audio_chunks
 from transcribe_ai import transcribe_audio
@@ -41,7 +46,7 @@ def transcribe_video(video_location: pl.Path,
         print('Creating transcription for chunk ' + str(audio_chunk_number))
         chunk_transcription = transcribe_audio(audio_location=audio_chunk)
 
-        with open(output_location, 'a') as f:
+        with open(output_location, 'a', encoding='utf_8') as f:
             f.write('PART ' + str(audio_chunk_number) + '\n')
 
         current_time_offset = audio_chunk_number * chunk_duration
@@ -50,8 +55,71 @@ def transcribe_video(video_location: pl.Path,
             # if we do not do this, the resulting timestamps will start counting at the beginning
             # of each chunk
             print_time = time.strftime("%H:%M:%S", time.gmtime(segment.start + current_time_offset))
-            with open(output_location, 'a') as f:
+            with open(output_location, 'a', encoding='utf_8') as f:
                 f.write(print_time + ' ' + segment.text + '\n')
 
-if __name__ == '__main__':
-    transcribe_video(video_location=pl.Path("PATH/TO/YOUR/VIDEO.mp4"))
+
+def transcribe_presentation(presentation_location: pl.Path,
+                            output_location: pl.Path | None = None
+                            ) -> None:
+    '''
+    Transcribes a presentation in .pptx format by accessing the audio files of single slides. This makes
+    it possible to create a transcript that differentiates by slide. Each single audio file (each audio 
+    file on each slide) should not be larger than 25 MB due to API limitations.
+
+    Args:
+        presentation_location (Path):   path to the input presentation. Must be a .pptx file.
+        output_location (Path | None):  path to the output file to save the transcription in. If None, transcription will be saved
+                                        in the same directory as the input video under the same name in the .txt format.
+    '''
+    if presentation_location.suffix != '.pptx':
+        raise ValueError('Input must be a .pptx presentation!')
+
+    if output_location is None:
+        output_location = create_file_in_same_dir(input_file_location=presentation_location, file_ending='_transcript.txt')
+
+    # copy pptx file to a zip archive
+    zip_location = presentation_location.parent / (presentation_location.stem + '_zip.zip')
+    shutil.copy(presentation_location, zip_location)
+
+    # extract zip file and delete it afterwards
+    extract_location = zip_location.with_suffix('')
+    extract_location.mkdir(exist_ok=True)
+    with zipfile.ZipFile(zip_location, "r") as z:
+        z.extractall(extract_location)
+    os.remove(zip_location)
+
+    # iterate over all m4a files in the extracted ppt/media directory
+    # since these are the slides' audios
+    # we use a sorting approach here and remove the 'media' string beforehand since
+    # we want to iterate using the right order. Without the sorting media10 and media11
+    # would come directly after media1. However, we want to access media2 after media1
+    media_location = extract_location / 'ppt' / 'media'
+    total_duration = 0
+    for slide_number, audio_file in enumerate(sorted(media_location.glob('**/*.m4a'), key=lambda file: int(file.stem.replace("media", "")))):
+
+        print('Creating transcription for slide ' + str(slide_number + 1))
+        slide_transcription = transcribe_audio(audio_location=audio_file)
+
+        with open(output_location, 'a', encoding='utf_8') as f:
+            f.write('SLIDE ' + str(slide_number + 1) + '\n')
+
+        for segment in slide_transcription.segments: # type: ignore
+            # we need to add the current total_duration to get the actual time in the original video
+            # if we do not do this, the resulting timestamps will start counting at the beginning
+            # of each slide
+            print_time = time.strftime("%H:%M:%S", time.gmtime(segment.start + total_duration))
+            with open(output_location, 'a', encoding='utf_8') as f:
+                f.write(print_time + ' ' + segment.text + '\n')
+
+        clip = AudioFileClip(audio_file)
+        total_duration += clip.duration
+        clip.close()
+
+    # tidy up and remove extraction folder
+    shutil.rmtree(extract_location)
+
+
+# if __name__ == '__main__':
+#     transcribe_video(video_location=pl.Path("PATH/TO/YOUR/VIDEO.mp4"))
+#     transcribe_presentation(presentation_location=pl.Path("PATH/TO/YOUR/PRESENTATION.pptx"))
